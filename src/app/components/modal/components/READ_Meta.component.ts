02b076ec93a9a673b24a61f57be886a6101e9700
Fabien Del Olmo <fabien.delolmo@gmail.com>
2019-04-12 10:35:05 +0200

    Moment.js pour l'affichage des dates de modifications/créations

--- src/app/components/modal/components/READ_Meta.component.ts
+++ src/app/components/modal/components/READ_Meta.component.ts
@@ -6,8 +6,18 @@ import { Component, Input } from '@angular/core';
   	<ion-card  color="primary">
 		<ion-card-content>
 			Version : {{meta?.version}} <br>
-			Modifié le : {{meta.timestamp === 0 ? 'Pas encore créé'  : meta.timestamp | date:'dd/MM/yyyy HH:mm'}}
-			<!-- | date:'dd/MM/yyyy HH:mm'-->
+			<span *ngIf="meta.timestamp === 0">
+				En cours de création
+			</span>
+			<span *ngIf="meta.timestamp !== 0">
+				{{meta?.version > 1 ? 'Modifié' : 'Créé'}}
+			</span>
+			<span *ngIf="displayCode && meta.timestamp !== 0 ">
+			 le : {{ meta.timestamp | date:'dd/MM/yyyy HH:mm'}}
+			</span>
+			<span *ngIf="!displayCode && meta.timestamp !== 0" >
+			 {{  meta.timestamp | amTimeAgo}}
+			</span>
 			<br> Par : {{meta?.user ? meta?.user : 'Moi même'}}
 		</ion-card-content>
 	</ion-card>
@@ -15,4 +25,5 @@ import { Component, Input } from '@angular/core';
 })
 export class ReadMeta {
     @Input() meta;
+    @Input() displayCode;
 }
