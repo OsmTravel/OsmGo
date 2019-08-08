37c113926c3d864bac68d4652efd6cd64220b71e
Fabien Del Olmo <fabien.delolmo@gmail.com>
2019-08-08 07:15:15 +0200

    Fix translation (#21, #22)

--- src/app/services/osmApi.service.ts
+++ src/app/services/osmApi.service.ts
@@ -22,7 +22,7 @@ import {
 @Injectable({ providedIn: 'root' })
 export class OsmApiService {
 
-    isDevServer = false; // dev serveur TODO
+    isDevServer = false;
     urlsOsm = {
         prod: { 'api': 'https://api.openstreetmap.org', 'overpass': 'https://overpass-api.de/api/interpreter' },
         dev: { 'api': 'https://master.apis.dev.openstreetmap.org', 'overpass': '' }
