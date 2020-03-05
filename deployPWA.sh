52d4da68f7da7f703b4353b0e21847734b6d6991
Fabien Del Olmo <fabien.delolmo@gmail.com>
2020-03-05 11:20:53 +0100

    Target ES5 : compatibility with Android 6 (<)

--- deployPWA.sh
+++ deployPWA.sh
@@ -13,7 +13,7 @@ mkdir -p $serverOsmGoPath
 
 "
 node ./incrementVersion.js
-node ./osmGoAdmin/backend/updatei18nMetadata.js
+# node ./osmGoAdmin/backend/updatei18nMetadata.js
 
 rm -r www
 ng build --prod && cd ./www && rsync -e "ssh -p $serverPort" -avz ./ $serverUser@$serverHost:$serverOsmGoPath
