ea8eaaddf75a0c9aeff05f99e391a75920fcf241
Fabien Del Olmo <fabien.delolmo@gmail.com>
2021-10-05 19:16:21 +0200

    The menu animation was too slow

--- src/app/components/menu/menu.animations.ts
+++ src/app/components/menu/menu.animations.ts
@@ -12,20 +12,20 @@ export const menuAnimations = [
         transition(':enter', [
             query('.menuContent', [
                 style({ transform: 'translateX(-100%)' }),
-                animate('300ms ease-in')
+                animate('100ms ease-in')
             ]),
             query('.backdrop', [
                 style({ opacity: '0.34' }),
-                animate('300ms')
+                animate('100ms')
             ]),
         ]),
         transition(':leave', [
             query('.menuContent', [
-                animate('0.3s ease-out', style({ transform: 'translateX(-100%)' }))
+                animate('0.1s ease-out', style({ transform: 'translateX(-100%)' }))
             ]),
 
             query('.backdrop', [
-                animate('0.3s ease-out', style({ opacity: '0' }))
+                animate('0.1s ease-out', style({ opacity: '0' }))
             ]),
         ])
     ])
