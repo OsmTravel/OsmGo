01aae3a8b1330fa654cb53349b83e2d89d48f7e6
Binnette <binnette@gmail.com>
2022-06-01 18:50:16 +0200

    addFieldsFromTagInfo.js: add doc

--- scripts/addFieldsFromTagInfo.js
+++ scripts/addFieldsFromTagInfo.js
@@ -1,3 +1,11 @@
+/**
+ * Update the file 'tagsAndPresets/presets.json' with possible values from taginfo
+ *
+ * 1. Read the list of OSM keys from file 'tagsAndPresets/presets.json'
+ * 2. If the 'preset key' is of type "list/select", the script will call taginfo to get possible values
+ * 3. Write the possible values in the same file 'tagsAndPresets/presets.json'
+ */
+
 const path = require('path');
 const fs = require('fs');
 const stringify = require("json-stringify-pretty-compact")
@@ -14,19 +22,19 @@ const presetsOsmgo = JSON.parse(fs.readFileSync(presetsOsmgoPath, 'utf8'));
 
 const getOptionsFromTagInfo = async (key, nb = 30) => {
     const url = `https://taginfo.openstreetmap.org/api/4/key/values?key=${key}&rp=${nb}&sortname=count&sortorder=desc`
-    
+
     const rep = await rp(url);
     // console.log
     const data = JSON.parse(rep).data;
-    if (!data){
+    if (!data) {
         return null;
     }
     // console.log(data);
     let options = [];
 
-    for (let d of data){
-        if ( !/;/.test(d.value) ){
-            let opt = {"v": d.value,  "lbl": {"en": d.value} } 
+    for (let d of data) {
+        if (!/;/.test(d.value)) {
+            let opt = { "v": d.value, "lbl": { "en": d.value } }
             options.push(opt)
         }
     }
@@ -35,23 +43,23 @@ const getOptionsFromTagInfo = async (key, nb = 30) => {
 }
 
 
-const run = async() => {
-    for (let k in presetsOsmgo){
+const run = async () => {
+    for (let k in presetsOsmgo) {
         const currentPreset = presetsOsmgo[k];
-        if (['list','select'].includes(currentPreset.type)){
-            
-            if (!currentPreset.options && !currentPreset.optionsFromJson){
+        if (['list', 'select'].includes(currentPreset.type)) {
+
+            if (!currentPreset.options && !currentPreset.optionsFromJson) {
                 // console.log(currentPreset.key)
-              let options =  await getOptionsFromTagInfo(currentPreset.key)
-              if(options){
-                currentPreset['options'] = options.slice(0, 30);
-                console.log(currentPreset['options']);
-              }
-              
+                let options = await getOptionsFromTagInfo(currentPreset.key)
+                if (options) {
+                    currentPreset['options'] = options.slice(0, 30);
+                    console.log(currentPreset['options']);
+                }
+
                 // console.log(currentPreset);
             }
-    
-    
+
+
         }
     }
 
