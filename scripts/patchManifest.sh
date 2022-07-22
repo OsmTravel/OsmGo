f2818048357d6e6fc0e7a1651857e86fd3e90774
dotcs <git@dotcs.me>
2022-07-22 22:49:49 +0200

    Add script to post-process webmanifest file in builds for ghpages

---
+++ scripts/patchManifest.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+set -ex
+
+USAGE="""\
+Patch service worker webmanifest file to set a different scope and start_url to
+work with a setup in which the application is not served from the root
+directory.
+
+This file is intended to be used after the npm build:ghpages script has
+completed.
+"""
+
+WEBMANIFEST_PATH="www/manifest.webmanifest"
+BASE_URL="/OsmGo"
+
+sed -i "s#\"scope\": \"/\"#\"scope\": \"${BASE_URL}\"#" "${WEBMANIFEST_PATH}"
+sed -i "s#\"start_url\": \"/\"#\"start_url\": \"${BASE_URL}\"#" "${WEBMANIFEST_PATH}"
+sed -i "s#\"id\": \"/\"#\"id\": \"${BASE_URL}\"#" "${WEBMANIFEST_PATH}"
