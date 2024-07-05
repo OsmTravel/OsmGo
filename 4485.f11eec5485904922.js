081c8374c304835ba0801ed4318d84285ead22e4
DoFabien <DoFabien@users.noreply.github.com>
2024-07-05 14:20:12 +0000

    deploy: b6a651a8b5744797851ff3b385b44538d7bd7983

---
+++ 4485.f11eec5485904922.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4485],{4485:(_,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>r});var s=e(5861),o=e(2726);class r extends o.Uw{show(u){return(0,s.Z)(function*(){})()}hide(u){return(0,s.Z)(function*(){})()}}}}]);
